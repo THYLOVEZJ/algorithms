13940dd9f2ad97d1bac2c094bb8a76e373527bd5
THYLOVEZJ <1105864486@qq.com>
2023-04-06 16:46:04 +0800

    test

---
+++ basic/test.java
@@ -0,0 +1,11 @@
+package com.algorithms.basic;
+
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int res = sc.nextInt();
+        System.out.println(res);
+    }
+}
